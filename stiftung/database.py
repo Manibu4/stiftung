a23f5030940e53f8b05529541937d7e988458c57
Manibu4 <manuela.utzinger@usb.ch>
2017-09-04 18:48:40 +0200

    fehler gha

--- stiftung/database.py
+++ stiftung/database.py
@@ -25,11 +25,12 @@ def edit_foundation(conn, values):
       address=?, pnumber=?, mail=?, website=?, contactperson=?, purpose=?,
       kindofboost=?, sum=?, currency=?, broadness=?, acadDegree=?, condElse=?,
       deadline=?, variabel=?, fix=?, pending=?, noInfo=?, resContact=?,
-      notes=?, timeContact=?, lastChange=?, deleted=0 WHERE id=?""", \
+      notes=?, timeContact=?, lastChange=?, deleted=? WHERE id=?""", \
       (values[0], values[1], values[2], values[3], values[4], values[5], \
        values[6], values[7], values[8], values[9], values[10], values[11], \
        values[12], values[13], values[14], values[15], values[16], values[17], \
-       values[18], values[19], values[20], values[21], values[22], values[23] ))
+       values[18], values[19], values[20], values[21], values[22], values[23],\
+       values[24] ))
     conn.commit()
 
 
